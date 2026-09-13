c33be85c581bb1b1dd69ca5fe76dccb6a0521ed1
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2026-09-12 20:54:37 -0500

    Prueba matematica lista

---
+++ src/Taller3/PruebaMatematica.java
@@ -0,0 +1,10 @@
+package Taller3;
+
+public class PruebaMatematica {
+    public static void main(String[] args) {
+        System.out.println("Suma: " + Matematicas.suma(10, 5));
+        System.out.println("Resta: " + Matematicas.resta(10, 5));
+        System.out.println("Multiplicación: " + Matematicas.multiplicacion(10, 5));
+        System.out.println("División: " + Matematicas.division(10, 5));
+    }
+}
