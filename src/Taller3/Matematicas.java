ad8a8205501244a6230867fae17bd10b59bd85f6
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2026-09-12 20:54:32 -0500

    Ejercicio de matematicas listo

---
+++ src/Taller3/Matematicas.java
@@ -0,0 +1,23 @@
+package Taller3;
+
+public class Matematicas {
+    public static double suma(double a, double b) {
+        return a + b;
+    }
+
+    public static double resta(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicacion(double a, double b) {
+        return a * b;
+    }
+
+    public static double division(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: No se puede dividir entre cero.");
+            return 0;
+        }
+        return a / b;
+    }
+}
